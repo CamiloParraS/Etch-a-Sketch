fed620a86219fad4d799d3faedf8fbac2c448a24
CamiloParraS <juanparracsfn@gmail.com>
2025-07-10 15:19:20 -0500

    Adeed the change Grid Size button

--- app.js
+++ app.js
@@ -13,11 +13,19 @@ for (let i = 0; i < gridSize * gridSize; i++) {
   // GRID STYLING
   cell.classList.add("grid-cell");
   cell.style.border = "1px solid #ddd";
-  cell.style.borderRadius = "2px";
+  cell.style.borderRadius = "0.3rem";
   cell.style.minWidth = "4rem";
   cell.style.minHeight = "4rem";
   cell.style.margin = "0.2rem";
   cell.style.backgroundColor = "white";
+  cell.style.transition = "background-color 0.2s ease-in-out";
   container.appendChild(cell);
-}
 
+  // HOVER STYLE
+  cell.addEventListener("mouseover", () => {
+    cell.style.backgroundColor = "#333";
+  });
+//   cell.addEventListener("mouseout", () => {
+//     cell.style.backgroundColor = "WHITE";
+//   });
+}
