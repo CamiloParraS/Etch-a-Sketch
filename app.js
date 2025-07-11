197402aa0c00f06d7fe50b4c81007f44c56e519f
CamiloParraS <juanparracsfn@gmail.com>
2025-07-11 14:29:27 -0500

    changed drawing logic, now a square gets draw when you hold the maouse down

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // ========== GLOBAL VARIABLES ==========
 
 const gridSize = 8; // DEFAULT VALUE
+const GRID_PIXEL_WIDTH = 400;
+let isMouseDown = false;
 
 // ========== DOM ELEMENTS ==========
 
@@ -13,26 +15,24 @@ const gridSizeBtn = document.querySelector("#grid-size-btn");
 function createGrid(size) {
   container.innerHTML = "";
   console.log("Creating grid");
+  const cellDimension = GRID_PIXEL_WIDTH / size;
+
   for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
-    // CELL STYLING
     cell.classList.add("grid-cell");
+
+    // CELL STYLING
+    cell.style.width = `${cellDimension}px`;
+    cell.style.height = `${cellDimension}px`;
+    cell.style.boxSizing = "border-box";
     cell.style.border = "1px solid #ddd";
-    cell.style.borderRadius = "0.3rem";
-    cell.style.minWidth = "4rem";
-    cell.style.minHeight = "4rem";
-    cell.style.margin = "0.2rem";
+    //cell.style.borderRadius = "0.3rem";
+    //cell.style.margin = "0.01rem";
     cell.style.backgroundColor = "white";
     cell.style.transition = "background-color 0.2s ease-in-out";
     container.appendChild(cell);
 
     // HOVER STYLE
-    cell.addEventListener("mouseover", () => {
-      cell.style.backgroundColor = "#333";
-    });
-    //   cell.addEventListener("mouseout", () => {
-    //     cell.style.backgroundColor = "WHITE";
-    //   });
     cell.addEventListener("click", () => {
       cell.style.backgroundColor = "white";
     });
@@ -40,9 +40,28 @@ function createGrid(size) {
 }
 
 // ========== EVENT LISTENERS ==========
+
+container.addEventListener("mousedown", (e) => {
+  isMouseDown = true;
+  if (e.target.classList.contains("grid-cell")) {
+    e.target.style.backgroundColor = "#333";
+  }
+  e.preventDefault();
+});
+
+window.addEventListener("mouseup", () => {
+  isMouseDown = false;
+});
+
+container.addEventListener("mouseover", (e) => {
+  if (isMouseDown && e.target.classList.contains("grid-cell")) {
+    e.target.style.backgroundColor = "#333";
+  }
+});
+
 gridSizeBtn.addEventListener("click", () => {
-  const size = Number(prompt("Enter the size for the grid (1 - 50)"));
-  if (size >= 1 && size <= 50) {
+  const size = Number(prompt("Enter the size for the grid (1 - 100)"));
+  if (size >= 1 && size <= 100) {
     createGrid(size);
   } else {
     alert("Invalid size. Please enter a number between 1 and 100.");
